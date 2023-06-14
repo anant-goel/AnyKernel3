4bd254901f05540563f6fb95895c5da85238c823
anant-goel <100245626+anant-goel@users.noreply.github.com>
2023-06-14 06:48:01 +0530

    Updated anykernel.sh for Veux

--- anykernel.sh
+++ anykernel.sh
@@ -10,12 +10,9 @@ do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
-device.name5=
-supported.versions=
+device.name1=veux
+device.name2=peux
+supported.versions=9-13
 supported.patchlevels=
 '; } # end properties
 
